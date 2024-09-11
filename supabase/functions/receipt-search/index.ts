dd2859cbd7571c844c7459012addead0ef584da1
itsrofly <rocketeman1234@gmail.com>
2024-09-11 19:37:22 +0100

    Rate limit receipt search

--- supabase/functions/receipt-search/index.ts
+++ supabase/functions/receipt-search/index.ts
@@ -1,13 +1,26 @@
 import "jsr:@std/dotenv/load";
 import { createClient } from "jsr:@supabase/supabase-js@2";
+import { Ratelimit } from "https://cdn.skypack.dev/@upstash/ratelimit@2.0.2";
+import { Redis } from "https://esm.sh/@upstash/redis@1.34.0";
 
-const supabase = createClient(Deno.env.get("SUPABASE_URL")!, Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!)
+
+const supabase = createClient(
+  Deno.env.get("SUPABASE_URL")!,
+  Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!
+);
 
 interface Api {
   token: string,
   merchant: string,
   region: string
 }
+
+// Setup Redis
+const redis = new Redis({
+  url: Deno.env.get("RECEIPT_SEARCH_URL")!,
+  token: Deno.env.get("RECEIPT_SEARCH_TOKEN")!,
+});
+
 
 Deno.serve({
   onListen() {
@@ -26,6 +39,21 @@ Deno.serve({
     const { data: { user }, error } = await supabase.auth.getUser(payload.token);
     if (error | !user) return new Response("No Session Found token:"
       + payload.token, { status: 401 });
+
+    // Create a new ratelimiter that allows 50 requests per 43200 minutes (approximately 30 days)
+    const ratelimit = new Ratelimit({
+      redis,
+      limiter: Ratelimit.slidingWindow(50, "43200 m"), // 50 requests per 30 days
+      analytics: true,
+    });
+
+    const identifier = user.id;
+    const { success } = await ratelimit.limit(identifier);
+
+    if (!success) {
+      return new Response("Limit exceeded", { status: 429 });
+    }
+
 
     // Fetch user data
     const fetchUser = await supabase.from("accounts").select("customer_id").eq('id', user.id);
